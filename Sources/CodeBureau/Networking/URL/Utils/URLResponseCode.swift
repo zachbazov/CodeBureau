6eec2720029434b33bcf97e250adb7c59afaf58a
zachbazov <zachbazov@gmail.com>
2023-09-23 20:06:15 +0300

    Implemented `URLResponseCode` in `URL` module.

--- Sources/CodeBureau/Networking/URL/Utils/URLResponseCode.swift
+++ Sources/CodeBureau/Networking/URL/Utils/URLResponseCode.swift
@@ -1,8 +1,16 @@
 //
-//  File.swift
-//  
+//  URLResponseCode.swift
+//
 //
 //  Created by Developer on 23/09/2023.
 //
 
 import Foundation
+
+public enum URLResponseCode: Int {
+    case ok                     = 200
+    case badRequest             = 400
+    case unauthorized           = 401
+    case notFound               = 404
+    case internalServerError    = 500
+}
