ddd6703760a629a718b925de861467c2a5b2a4b7
zachbazov <zachbazov@gmail.com>
2023-09-29 15:08:45 +0300

    Deleted `ResponsePersistable`.

--- Sources/CodeBureau/Contracts/Data Persisting/ResponsePersistable.swift
+++ Sources/CodeBureau/Contracts/Data Persisting/ResponsePersistable.swift
@@ -5,6 +5,7 @@
 //  Created by Developer on 29/09/2023.
 //
 
+import Foundation
 import CoreData
 
 public protocol ResponsePersistable {
@@ -12,9 +13,10 @@ public protocol ResponsePersistable {
     associatedtype EntitiyType: NSManagedObject
     associatedtype RequestType: Decodable
     associatedtype ResponseType: Decodable
+    associatedtype ErrorType: Error
     
     func fetchRequest(for request: RequestType) -> NSFetchRequest<EntitiyType>
-    func fetchResponse(for request: RequestType, completion: @escaping (Result<ResponseType?, CoreDataStoreError>) -> Void)
+    func fetchResponse(for request: RequestType, completion: @escaping (Result<ResponseType?, ErrorType>) -> Void)
     func saveResponse(_ response: ResponseType, withRequest request: RequestType)
     func deleteResponse(withRequest request: RequestType, in context: NSManagedObjectContext)
 }
